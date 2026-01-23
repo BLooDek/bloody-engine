fb388c9ee300f5987ccea2425c0781473cd1e086
Pawel Michalak <pawelmichalak98@gmail.com>
2026-01-23 19:59:59 +0100

    fixed imports

--- src/public-api.ts
+++ src/public-api.ts
@@ -1,14 +1,14 @@
 // Core graphics device
-export { GraphicsDevice } from "./grahpic-device";
+export { GraphicsDevice } from "./core/grahpic-device";
 
 // Rendering context interface and implementations
 export type {
   RenderingContext,
   RenderingContextOptions,
 } from "./rendering/rendering-context";
-export { BrowserRenderingContext } from "./browser-context";
-export { NodeRenderingContext } from "./node-context";
-export { RenderingContextFactory } from "./rendering-context-factory";
+export { BrowserRenderingContext } from "./platforms/browser/browser-context";
+export { NodeRenderingContext } from "./platforms/node/node-context";
+export { RenderingContextFactory } from "./rendering/rendering-context-factory";
 
 // Shader abstraction
 export { Shader } from "./core/shader";
@@ -19,5 +19,5 @@ export { Texture } from "./core/texture";
 // Buffer management
 export { VertexBuffer, IndexBuffer } from "./core/buffer";
 // Batch rendering
-export { BatchRenderer } from "./batch-renderer";
-export type { QuadInstance } from "./batch-renderer";
+export { BatchRenderer } from "./rendering/batch-renderer";
+export type { QuadInstance } from "./rendering/batch-renderer";
